429b8c5ffc6a8dced6b695302a7c54b7c2309574
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2011-07-29 20:57:11 -0400

    Improved main menu

--- lib/Games/SolarConflict/Controller/MainMenu.pm
+++ lib/Games/SolarConflict/Controller/MainMenu.pm
@@ -2,32 +2,58 @@ package Games::SolarConflict::Controller::MainMenu;
 use Mouse;
 use SDL::Event;
 use SDL::Events;
+use SDLx::Text;
 
 with 'Games::SolarConflict::Roles::Controller';
+
+has title => (
+    is      => 'ro',
+    isa     => 'SDLx::Text',
+    builder => '_build_title',
+);
+
+sub _build_title {
+    my ($self) = @_;
+
+    return SDLx::Text->new(
+        color   => 0xFFFFFFFF,
+        size    => 96,
+        text    => 'Solar Conflict',
+        h_align => 'center',
+        x       => $self->game->app->w / 2,
+    );
+}
 
 sub handle_show {
     my ( $self, $delta, $app ) = @_;
 
     $app->draw_rect( [ 0, 0, $app->w, $app->h ], 0x000000FF );
-    $app->draw_gfx_text( [ 0, 0 ],  0xFFFFFFFF, 'SolarConflict' );
-    $app->draw_gfx_text( [ 0, 10 ], 0xFFFFFFFF, 'Press 1 for single player' );
-    $app->draw_gfx_text( [ 0, 20 ], 0xFFFFFFFF, 'Press 2 for two player' );
 
-    $app->draw_gfx_text( [ 0, 100 ], 0xFFFFFFFF, 'Player 1' );
-    $self->game->spaceship1->sprite->draw_xy( $app, 0, 110 );
-    $app->draw_gfx_text( [ 0, 140 ], 0xFFFFFFFF, 'Q - Fire Torpedo' );
-    $app->draw_gfx_text( [ 0, 150 ], 0xFFFFFFFF, 'W - Accelerate' );
-    $app->draw_gfx_text( [ 0, 160 ], 0xFFFFFFFF, 'A - Rotate CCW' );
-    $app->draw_gfx_text( [ 0, 170 ], 0xFFFFFFFF, 'D - Rotate CW' );
-    $app->draw_gfx_text( [ 0, 180 ], 0xFFFFFFFF, 'S - Hyperspace' );
+    $self->title->write_to($app);
 
-    $app->draw_gfx_text( [ 200, 100 ], 0xFFFFFFFF, 'Player 2' );
-    $self->game->spaceship2->sprite->draw_xy( $app, 200, 110 );
-    $app->draw_gfx_text( [ 200, 140 ], 0xFFFFFFFF, 'U - Fire Torpedo' );
-    $app->draw_gfx_text( [ 200, 150 ], 0xFFFFFFFF, 'I - Accelerate' );
-    $app->draw_gfx_text( [ 200, 160 ], 0xFFFFFFFF, 'J - Rotate CCW' );
-    $app->draw_gfx_text( [ 200, 170 ], 0xFFFFFFFF, 'L - Rotate CW' );
-    $app->draw_gfx_text( [ 200, 180 ], 0xFFFFFFFF, 'K - Hyperspace' );
+    $app->draw_gfx_text( [ 400, 210 ],
+        0xFFFFFFFF, 'Press 1 for single player' );
+    $app->draw_gfx_text( [ 400, 220 ], 0xFFFFFFFF, 'Press 2 for two player' );
+
+    my ( $x1, $y1 ) = ( 300, 300 );
+
+    $app->draw_gfx_text( [ $x1, $y1 ], 0xFFFFFFFF, 'Player 1' );
+    $self->game->spaceship1->sprite->draw_xy( $app, $x1, $y1 + 10 );
+    $app->draw_gfx_text( [ $x1, $y1 + 40 ], 0xFFFFFFFF, 'Q - Fire Torpedo' );
+    $app->draw_gfx_text( [ $x1, $y1 + 50 ], 0xFFFFFFFF, 'W - Accelerate' );
+    $app->draw_gfx_text( [ $x1, $y1 + 60 ], 0xFFFFFFFF, 'A - Rotate CCW' );
+    $app->draw_gfx_text( [ $x1, $y1 + 70 ], 0xFFFFFFFF, 'D - Rotate CW' );
+    $app->draw_gfx_text( [ $x1, $y1 + 80 ], 0xFFFFFFFF, 'S - Hyperspace' );
+
+    my ( $x2, $y2 ) = ( 600, 300 );
+
+    $app->draw_gfx_text( [ $x2, $y2 + 00 ], 0xFFFFFFFF, 'Player 2' );
+    $self->game->spaceship2->sprite->draw_xy( $app, $x2, $y2 + 10 );
+    $app->draw_gfx_text( [ $x2, $y2 + 40 ], 0xFFFFFFFF, 'U - Fire Torpedo' );
+    $app->draw_gfx_text( [ $x2, $y2 + 50 ], 0xFFFFFFFF, 'I - Accelerate' );
+    $app->draw_gfx_text( [ $x2, $y2 + 60 ], 0xFFFFFFFF, 'J - Rotate CCW' );
+    $app->draw_gfx_text( [ $x2, $y2 + 70 ], 0xFFFFFFFF, 'L - Rotate CW' );
+    $app->draw_gfx_text( [ $x2, $y2 + 80 ], 0xFFFFFFFF, 'K - Hyperspace' );
     $app->update();
 }
 
