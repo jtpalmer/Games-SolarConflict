c6598fc9f89f53fe13e6fc4724ac9cb040adeede
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2012-05-20 22:11:51 -0400

    Add darwin SDLPerl check, etc.

--- bin/solar-conflict.pl
+++ bin/solar-conflict.pl
@@ -1,9 +1,19 @@
 #!perl
 use strict;
 use warnings;
+
+BEGIN {
+    if ( $^O eq 'darwin' && $^X ne 'SDLPerl' ) {
+        exec 'SDLPerl', $0, @ARGV or die "Failed to exec SDLPerl: $!";
+    }
+}
+
 use Games::SolarConflict;
 
 # PODNAME: solar-conflict.pl
+# ABSTRACT: Play the game
 
 Games::SolarConflict->new->run();
+
+exit;
 
