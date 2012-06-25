0ed41c6657a02d7161a0e48940eb1752cdce2d18
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2012-06-25 18:47:24 -0400

    More docs

--- bin/solar-conflict.pl
+++ bin/solar-conflict.pl
@@ -18,3 +18,25 @@ Games::SolarConflict->new->run();
 
 exit;
 
+=pod
+
+=head1 SYNOPSIS
+
+    solar-conflict.pl
+
+=head1 DESCRIPTION
+
+This script starts the game.  It will open a 1024x768 pixel window (plus
+the size of the window frame).  Game controls are displayed on the menu
+screen.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<Games::SolarConflict>
+
+=back
+
+=cut
+
